ce3dad6468653c0522c8c7c63781ac36d3a20616
komosinskid <32815122+komosinskid@users.noreply.github.com>
2018-11-27 15:02:04 +0100

    Update test_ceteris_paribus.R

--- tests/testthat/test_ceteris_paribus.R
+++ tests/testthat/test_ceteris_paribus.R
@@ -26,15 +26,19 @@ explainer_rf <- explain(HR_rf, data = HRTest, y = HRTest)
 my_HR <- HRTest[1, ]
 
 lp_rf <- individual_variable_profile(explainer_rf,  my_HR)
-
+lp_rf2 <- individual_variable_profile(HR_rf, data = HR, new_observation = my_HR, y = my_HR$evaluation, variable_splits = NULL)
 
 test_that("Wrong input",{
   expect_error(individual_variable_profile(explainer_rf,  NULL))
+  expect_error(individual_variable_profile(HR_rf, data = NULL, new_observation = my_HR, variable_splits = NULL))
   expect_error(ceteris_paribus(apartments_rf_model))
 })
 
 test_that("Output format - individual_variable_profile",{
   expect_is(lp_rf, "individual_variable_profile_explainer")
   expect_is(lp_rf, "data.frame")
+  expect_is(lp_rf2, "individual_variable_profile_explainer")
+  expect_is(lp_rf2, "data.frame")
+
 })
 
